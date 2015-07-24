7ce2cfa40608c45b26974244fce038c6d22ea6dc
Sam Genduso <sam.genduso@gmail.com>
2015-07-24 17:08:11 -0600

    css normalized

--- public/javascripts/canvas.js
+++ public/javascripts/canvas.js
@@ -1,2 +1,22 @@
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
+
+(function() {
+    var canvas = document.getElementById('myCanvas'),
+    context = canvas.getContext('2d');
+    
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, false);
+
+    function resizeCanvas() {
+            canvas.width = window.innerWidth;
+            /**
+             * Your drawings need to be inside this function otherwise they will be reset when
+             * you resize the browser window and the canvas goes will be cleared.
+             */
+            drawStuff();
+    }
+    resizeCanvas();
+
+    function drawStuff() {
+            // do your drawing stuff here
+    }
+})();
