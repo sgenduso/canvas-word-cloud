8d12dcc4f0a5ad0b70ca2019ba6ba433be123b90
Sam Genduso <sam.genduso@gmail.com>
2015-07-26 19:44:41 -0600

    remove punctuation, filter common words

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var cloudFuncs = require('../lib/cloud.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Word Cloud' });
+  res.render('index');
+});
+
+router.post('/', function (req, res, next) {
+  var text = req.body.text_input;
+  var wordRanks = cloudFuncs.wordRanks(text);
+  res.render('index', {text: text});
 });
 
 module.exports = router;
